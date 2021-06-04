46e95e5b6e4412394dc934af3a86b7aeae10b790
Ji-Yong, Jeong <wldyd2356@gmail.com>
2021-06-05 01:13:50 +0900

    feat: jpa 적용 및 테스트

---
+++ src/main/java/com/wscrg/springboot/domain/posts/PostsRepository.java
@@ -0,0 +1,6 @@
+package com.wscrg.springboot.domain.posts;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface PostsRepository extends JpaRepository<Posts, Long> {
+}
